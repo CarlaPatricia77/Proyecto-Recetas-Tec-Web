973776e942a0809b1e1360aa4ccb44c0d2550c8a
CarlaPatricia77 <carla.copacalle@ucb.edu.bo>
2026-01-12 17:43:51 -0400

    Jsons agregados

--- Recetas.Api/obj/Debug/net9.0/Recetas.Api.AssemblyInfo.cs
+++ Recetas.Api/obj/Debug/net9.0/Recetas.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recetas.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f2d4de7b4a4251e8f3d84467508d69dd996431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133ce4fc29a3c1991963920f24dc015d0d953f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recetas.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recetas.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
